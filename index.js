e1d08ae1451ec2467a9b7d468feee3e5ad2d6d88
Kieran-Mahon <84115639+Kieran-Mahon@users.noreply.github.com>
2024-10-30 20:35:48 +1300

    Added mysql placeholders

--- index.js
+++ index.js
@@ -24,22 +24,30 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-//Connect to the database
-connection.connect((err) => {
-  //Make sure no errors
-  if (err) {
-    console.error('Error connecting to MySQL: ' + err.stack);
-    return;
-  }
-  console.log('Connected to MySQL as ID ' + connection.threadId);
-});
+function connectToDB() {
+  //Connect to the database
+  connection.connect((err) => {
+    //Make sure no errors
+    if (err) {
+      console.error('Error connecting to MySQL: ' + err.stack);
+      return false;
+    }
+    console.log('Connected to MySQL as ID ' + connection.threadId);
+    return true;
+  });
+}
 
 //Main API request - Selects speed data from the database
 app.get("/road-data", (req, res) => {
+  //Make sure there is a connection to the database
+  if (connectToDB() == false) {
+    res.status(500).send('Error connecting to database');
+    return;
+  }
 
   //Get the variables from the request
   const route = req.query.route;
-  const day = req.query.day;;
+  const day = req.query.day;
   const hour = req.query.hour;
 
   //Write a message to log to signal that someone requested
@@ -49,11 +57,10 @@ app.get("/road-data", (req, res) => {
   let query = 'SELECT at_data.speed_data.dataCount, at_data.road_data.latA, at_data.road_data.lonA, at_data.road_data.latB, ';
   query += 'at_data.road_data.lonB, at_data.road_data.roadName, at_data.speed_data.sumSpeed, at_data.road_data.maxSpeed ';
   query += 'FROM at_data.road_data inner join at_data.speed_data ON at_data.road_data.roadID = at_data.speed_data.roadID ';
-  query += 'WHERE at_data.road_data.roadID IN (SELECT roadID FROM at_data.route_data WHERE routeID = \'' + route + '\') ';
-  query += 'AND day = \'' + day + '\' AND hour = \'' + hour + '\';';
+  query += 'WHERE at_data.road_data.roadID IN (SELECT roadID FROM at_data.route_data WHERE routeID = ?) AND day = ? AND hour = ?;';
 
   //Execute the query
-  connection.query(query, (err, results) => {
+  connection.query(query, [route, day, hour], (err, results) => {
     //Check for any errors
     if (err) {
       console.error('Error executing query: ' + err.stack);
